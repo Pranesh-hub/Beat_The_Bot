239e24dc1286b0e7f170c36eedf86fefb84191f8
Pranesh-hub <praneshsenthilvel26@gmail.com>
2025-12-07 11:15:39 +0530

    Pong done

--- Assets/Scripts/ScoreUpdate.cs
+++ Assets/Scripts/ScoreUpdate.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
 
 public class ScoreUpdate : MonoBehaviour
 {
     // Start is called before the first frame update
+    public TextMeshProUGUI RightScoreText;
+    public int Right_score = 0;
+    public TextMeshProUGUI LeftScoreText;
+    public int Left_score = 0;
+    public bool isRight = true;
     void Start()
     {
-        
+        RightScoreText.text = "Right Player's Score: " + Right_score.ToString();
+        LeftScoreText.text = "Left Player's Score: " + Left_score.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        Debug.Log("Goal Scored");
+        if (collision.gameObject.CompareTag("Ball") && isRight)
+        {
+            Debug.Log("Left Player Scored");
+            Left_score++;
+            LeftScoreText.text = "Left Player's Score: " + Left_score.ToString();
+        }
+        else if (collision.gameObject.CompareTag("Ball") && !isRight)
+        {
+            Debug.Log("Right Player Scored");
+            Right_score++;
+            RightScoreText.text = "Right Player's Score: "+ Right_score.ToString();        
+        }       
     }
 }
