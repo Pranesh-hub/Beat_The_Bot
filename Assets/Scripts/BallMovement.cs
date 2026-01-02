8a92573fbed58acbb2f81e064f2c9a599797e1a5
Pratham Sharma <pratham15072007@gmail.com>
2026-01-02 22:23:35 +0530

    Pong Agent

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BallMovement : MonoBehaviour
+{
+    public float BallSpeed = 8f;
+    public Rigidbody2D rb;
+
+    private Vector2 lastFrameVelocity;
+
+    public GameManager gameManager;
+    void Start()
+    {
+        if (rb == null) rb = GetComponent<Rigidbody2D>();
+
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        rb.gravityScale = 0f;
+    }
+
+    void Update()
+    {
+        lastFrameVelocity = rb.velocity;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PongAgent agent = collision.collider.GetComponent<PongAgent>();
+            if (agent != null)
+            {
+                agent.AddReward(+0.005f);
+            }
+        }
+
+        if (!collision.gameObject.CompareTag("Wall") &&
+            !collision.gameObject.CompareTag("Player"))
+            return;
+
+        if (lastFrameVelocity.sqrMagnitude < 0.0001f) return;
+
+        Vector2 normal = Vector2.zero;
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            normal += collision.GetContact(i).normal;
+        }
+
+        if (normal == Vector2.zero) return;
+        normal.Normalize();
+
+        ContactPoint2D c = collision.GetContact(0);
+
+        float speed = lastFrameVelocity.magnitude;
+        Vector2 direction = lastFrameVelocity.normalized;
+
+        Vector2 reflectedDir = Vector2.Reflect(direction, normal).normalized;
+        rb.velocity = reflectedDir * speed;
+    }
+}
